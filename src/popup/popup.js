686f6d8b521d6ab5842e0ec6bbf8494499af75e9
Colin Mason <masonc789@gmail.com>
2022-10-05 14:54:16 -0400

    remove on-demand explosion

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,6 +1,4 @@
-document.getElementById("explodeTabs").onclick = () => {
-	explodeTabsWithStyle();
-};
+
 
 const selectElement = document.getElementById("dateOptions");
 
@@ -13,13 +11,6 @@ selectElement.addEventListener("change", event => {
 	chrome.storage.local.set({ date: event.target.value }, () => {});
 	chrome.extension.getBackgroundPage().console.log(chrome.storage.date);
 });
-
-function explodeTabsWithStyle() {
-	// alert("It's Midnight!\nYou know what that means...\n\nKABOOM!!!");
-	chrome.extension.getBackgroundPage().console.log("trigger explosion");
-	triggerExplosion();
-	setTimeout(closeTabs, 4000);
-}
 
 function closeTabs() {
 	closeAllTabs(true, false);
